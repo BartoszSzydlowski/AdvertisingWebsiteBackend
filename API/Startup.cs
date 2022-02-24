da86f8344ab097b08f8c8a210692ca99ab867e9b
Bartosz Szydłowski <bbartek311@gmail.com>
2022-02-24 14:24:23 +0100

    changed cors settings

--- API/Startup.cs
+++ API/Startup.cs
@@ -124,11 +124,7 @@ namespace API
             app.UseCors(builder => builder
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .WithOrigins(
-                    "http://localhost:3000",
-                    "https://advertising-website-frontend-git-main-bartoszszydlowski.vercel.app",
-                    "https://advertising-website-frontend.vercel.app/",
-                    "https://advertising-website-frontend-bartoszszydlowski.vercel.app/")
+                .SetIsOriginAllowed(_ => true)
                 .AllowCredentials()
                 );
 
